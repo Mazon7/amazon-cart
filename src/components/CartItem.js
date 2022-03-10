a07dabb8c5f4a12f6bb57503906ab09ede216074
Max <maximgusho7@gmail.com>
2022-03-10 18:43:32 +0800

    ADD: CartItems rendered

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,31 +1,28 @@
 import React from "react";
 import "./CartItem.css";
 
-function Cartitem({ quantity }) {
+function Cartitem({ title, stock, image, price, quantity }) {
   return (
     <div className="CartItem">
       <div className="CartItem-image">
-        <img
-          src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mbp-spacegray-select-202011_GEO_SG?wid=1280&hei=1190&fmt=jpeg&qlt=80&.v=1632948873000"
-          alt="laptop"
-        />
+        <img src={process.env.PUBLIC_URL + "/items/" + image} alt="laptop" />
       </div>
       <div className="CartItem-info">
         <div className="info-title">
-          <h2>Macbook Pro</h2>
+          <h2>{title}</h2>
         </div>
-        <div className="info-stock">In stock</div>
+        <div className="info-stock">{stock}</div>
         <div className="item-actions">
           <div className="item-quantity">
             <select name="quantity" id="quantity">
               <option value={quantity}>Qty: {quantity}</option>
             </select>
           </div>
-          <div class="item-actions-divider">|</div>
+          <div className="item-actions-divider">|</div>
           <div className="item-delete">Delete</div>
         </div>
       </div>
-      <div className="CartItem-price">$1000</div>
+      <div className="CartItem-price">${price}</div>
     </div>
   );
 }
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -2,12 +2,23 @@ import React from "react";
 import CartItem from "./CartItem";
 import "./CartItems.css";
 
-function CartItems() {
+function CartItems({ items }) {
+  console.log("Inside CartItems component", items);
+
   return (
     <div className="CartItems">
       <h1>Shopping Cart</h1>
       <hr />
-      <CartItem />
+      {items.map((item, index) => (
+        <CartItem
+          key={index}
+          title={item.title}
+          stock={item.stock}
+          image={item.image}
+          price={item.price}
+          quantity={item.quantity}
+        />
+      ))}
     </div>
   );
 }
