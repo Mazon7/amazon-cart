8b00efdc03ae4f563618b0453a9600d63a341181
Max <maximgusho7@gmail.com>
2022-04-20 14:25:14 +0800

    Updates

---
+++ precache-manifest.695b83de01801300262fe990cf19c0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30551439e2c9982d39a7",
+    "url": "/amazon-cart/static/css/main.844144dd.chunk.css"
+  },
+  {
+    "revision": "30551439e2c9982d39a7",
+    "url": "/amazon-cart/static/js/main.0420c648.chunk.js"
+  },
+  {
+    "revision": "a4485fad45f6888c6292",
+    "url": "/amazon-cart/static/js/runtime~main.f13a7ffd.js"
+  },
+  {
+    "revision": "40a33165765a6aaf34f5",
+    "url": "/amazon-cart/static/js/2.09272cf8.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/amazon-cart/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "ac627948a3e87be2bde24878d7b62992",
+    "url": "/amazon-cart/index.html"
+  }
+];
